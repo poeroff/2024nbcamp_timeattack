9724c93986f29fa402639358ee70637e51abe66a
CHOI TAE YOUNG <chlxodud04@nave.rcom>
2024-02-08 23:58:03 +0900

    ....

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -28,13 +28,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     return user;
   }
   async acb(){
-    const user = await this.userService.findByEmail(payload.email);
-    if (_.isNil(user)) {
-      throw new NotFoundException('해당하는 사용자를 찾을 수 없습니다.');
-    }
-
-    return user;
-  }
+   asdasd
 
   
 
